da4ce4285efdc68b31b2ec720326b656f8ad3146
Realest Weirdo <ogbandav102@gmail.com>
2022-02-24 16:16:14 +0000

    added redux persist to ma app and using it to persist state and the cart content even on refresh or exiting or closing the application

--- src/index.js
+++ src/index.js
@@ -3,16 +3,20 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import {Provider} from 'react-redux';
-import Store from './redux/store';
+
+import {store, persistor} from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={Store}>
+    <Provider store={store}>
       <BrowserRouter>
+      <PersistGate persistor={persistor} >
         <App />
+      </PersistGate>
       </ BrowserRouter>
     </Provider>
   </React.StrictMode>,
